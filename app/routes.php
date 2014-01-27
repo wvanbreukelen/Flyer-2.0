065f19c70ddddc68853fea01fd2e8122c852ff91
wvanbreukelen <wdownload@xs4all.nl>
2014-01-27 22:14:37 +0100

    Removed a sample route

--- app/routes.php
+++ app/routes.php
@@ -2,9 +2,6 @@
 
 use Flyer\Components\Router\Route;
 
-Route::get('gay', function() {
-	echo 'The current page is {currentPage}';
-});
 Route::get('welkom', function() {
 	echo '<h2>Welkom</h2><p>Welkom in deze simpele applicatie die gebruik maakt van het Flyer Framework!</p><hr />';
 });
