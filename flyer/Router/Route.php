9eed57e5bf72bde2a40ea664806b3f7f0381f5cf
wvanbreukelen <wdownload@xs4all.nl>
2014-01-27 21:42:46 +0100

    Added documentation to the Route class

--- flyer/Router/Route.php
+++ flyer/Router/Route.php
@@ -4,27 +4,66 @@ namespace Flyer\Components\Router;
 
 use Flyer\Components\Router\RouteBuilder;
 
+/**
+ * The Route class responds to the RouteBuilder which builds the Route Query. This Query is passed to the Router
+ */
+
 class Route
 {
+	/**
+	 * Add a route by the HTTP GET method
+	 * 
+	 * @param string The URI where the Router can respond to
+	 * @param string The action if the Router matches with the given URI
+	 */
+	
 	public static function get($uri, $action)
 	{
 		Router::addRoute("GET", $uri, $action);
 	}
+	
+	/**
+	 * Add a route by the HTTP POST method
+	 * 
+	 * @param string The URI where the Router can respond to
+	 * @param string The action if the Router matches with the given URI
+	 */
 
 	public static function post($uri, $action)
 	{
 		Router::addRoute("POST", $uri, $action);
 	}
+	
+	/**
+	 * Add a route by the HTTP UPDATE method
+	 * 
+	 * @param string The URI where the Router can respond to
+	 * @param string The action if the Router matches with the given URI
+	 */
 
 	public static function update($uri, $action)
 	{
 		Router::addRoute("UPDATE", $uri, $action);
 	}
+	
+	/**
+	 * Add a route by the HTTP DELETE method
+	 * 
+	 * @param string The URI where the Router can respond to
+	 * @param string The action if the Router matches with the given URI
+	 */
 
 	public static function delete($uri, $action)
 	{
 		Router::addRoute("DELETE", $uri, $action);
 	}
+	
+	/**
+	 * Add a route by ANY HTTP method
+	 * 
+	 * @param string The URI where the Router can respond to
+	 * @param string The action if the Router matches with the given URI
+	 */
 
 	public static function any($uri, $action)
 	{
