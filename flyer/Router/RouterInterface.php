aec6c1c99c8d01275d11e753c17a7c2757121d27
wvanbreukelen <wdownload@xs4all.nl>
2014-01-28 21:51:47 +0100

    Added documentation to the RouterInterface

--- flyer/Router/RouterInterface.php
+++ flyer/Router/RouterInterface.php
@@ -4,6 +4,10 @@ namespace Flyer\Components\Router;
 
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * The router inferface has to been implemented in your router
+ */
+
 interface RouterInterface
 {
 	public function setRequest(Request $request);
