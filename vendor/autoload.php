577d8589297f1436af2556e2de478b1b44bbc0ae
Wiebe van Breukelen <wdownload@xs4all.nl>
2014-01-31 18:11:03 +0100

    Runned a composer update

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit3a6086cc0764b6f6d30c333bb2ddb179::getLoader();
+return ComposerAutoloaderInit1fc2e35359b18ac00875e2c2f22518d0::getLoader();
