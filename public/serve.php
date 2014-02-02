d02edb9919fc4aaaa54e6bcba2e61b106c25f624
wvanbreukelen <wdownload@xs4all.nl>
2014-02-02 13:23:41 +0100

    Enable error reporting, to make sure errors are showing

--- public/serve.php
+++ public/serve.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
 define('DS', DIRECTORY_SEPARATOR);
 $stack = explode(DS, getcwd());
 $array = array_pop($stack);
