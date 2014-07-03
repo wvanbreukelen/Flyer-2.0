ff766b4a4f95a540452fd8d7eb70f53727ca727e
wvanbreukelen <wvanbreukelen@users.noreply.github.com>
2014-07-03 21:27:49 +0200

    Update ViewEngine.php

--- flyer/View/ViewEngine.php
+++ flyer/View/ViewEngine.php
@@ -19,7 +19,7 @@ class ViewEngine
 	{
 		if (is_null($id))
 		{
-			return $this->render($view . '.html');
+			return $this->render($view . '.php');
 		}
 
 		return Registry::get('application.view.compiler')->compile($id, $view);
