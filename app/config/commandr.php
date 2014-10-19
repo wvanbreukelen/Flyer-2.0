21e382b7ee33c1bb237f78519cfd0a89b87a788d
wvanbreukelen <wdownload@xs4all.nl>
2014-10-19 17:17:12 +0200

    Changed commandr.php

--- app/config/commandr.php
+++ app/config/commandr.php
@@ -2,14 +2,14 @@
 
 $config = array(
 	'commands' => array(
-		'Commandr\Commands\TestCommand',
 		'Commandr\Commands\HelpCommand',
 		'Flyer\Components\Router\Console\SimulateRouteCommand',
+		'Flyer\Components\Router\Console\RouteListCommand',
 	),
 	
 	'errors' => array(
 		'commandNotFound' => "This command cannot been found.",
-		'notEnoughArguments' => "Command %s does not matches the amount of required arguments!",
+		'notEnoughArguments' => "Command %s does not matches the amount of required arguments, type help   [command] for help",
 	),
 );
 
