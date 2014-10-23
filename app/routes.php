4a52b56b9f7c618ad3d6def88dec52d49b9fd5e5
wvanbreukelen <wdownload@xs4all.nl>
2014-10-23 15:01:37 +0200

    Changed routes.php

--- app/routes.php
+++ app/routes.php
@@ -2,5 +2,6 @@
 
 // Your routing file
 
-Route::get('hello', 'HomeController@index');
+Route::get('/hello', 'HomeController@index');
 Route::get('test', 'TestController@test');
+Route::get('/', 'TestController@test');
