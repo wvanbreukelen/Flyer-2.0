18fc3d01925a590266d68b2476445f5036883210
wvanbreukelen <wdownload@xs4all.nl>
2014-10-23 15:01:13 +0200

    Using Filesystem package now

--- app/compilers/Blade/Engine.php
+++ app/compilers/Blade/Engine.php
@@ -4,6 +4,7 @@ namespace wvanbreukelen\Blade;
 
 use wvanbreukelen\Blade\BladeCompiler;
 use Flyer\Components\View\Compiler\Compiler;
+use File;
 
 class BladeEngine extends Compiler
 {
@@ -12,18 +13,16 @@ class BladeEngine extends Compiler
 
 	protected $values;
 
-	public function compile($view, array $values = array())
+	public function compile($contents, $view, array $values = array())
 	{
-		$contents = file_get_contents(APP . 'views' . DS . $view . '.php');
-
 		$this->blade = new BladeCompiler();
 		$this->contents = $this->blade->compile($contents);
 
-		if (!file_exists(APP . 'storage/cache' . DS . $view . '.php'))
+		if (!File::exists(APP . 'storage/cache' . DS .$view . '.php'))
 		{
-			file_put_contents(APP . 'storage/cache' . DS . $view . '.php', $this->contents);
+			File::write(APP . 'storage/cache' . DS . $view . '.php', $this->contents);
 		} else if ($this->contents != file_get_contents(APP . 'storage/cache' . DS . $view . '.php')) {
-			file_put_contents(APP . 'storage/cache' . DS . $view . '.php', $this->contents);
+			File::write(APP . 'storage/cache' . DS . $view . '.php', $this->contents);
 		}
 
 		return $this->build(APP . 'storage/cache' . DS . $view . '.php', $values);
